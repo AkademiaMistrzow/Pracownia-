92dafcbbd7663c8edf402c1d18bbfb02a1eae879
unknown <jedrek90@gmail.com>
2025-05-10 18:21:09 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,32 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menuContact = document.querySelector('#menu-contact');
+const menuOmnie = document.querySelector('#menu-omnie');
+const menuOferta = document.querySelector('#menu-oferta');
+const menuCennik = document.querySelector('#menu-cennik');
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
 menuContact.addEventListener('click', () => {
+    if (window.innerWidth < 800) {
+        close();
+    }
+});
+
+menuOmnie.addEventListener('click', () => {
+    if (window.innerWidth < 800) {
+        close();
+    }
+});
+
+menuOferta.addEventListener('click', () => {
+    if (window.innerWidth < 800) {
+        close();
+    }
+});
+
+menuCennik.addEventListener('click', () => {
     if (window.innerWidth < 800) {
         close();
     }
